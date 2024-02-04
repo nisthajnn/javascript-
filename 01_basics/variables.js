c95cd8ab39a0e36f5a5bc8c78cb8cb7562b39497
nisthajnn <nisthajnn1234@gmail.com>
2024-02-04 06:47:12 +0000

    learning variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,7 @@
+const accountId=1234;
+let accountName="abbac";
+var accountCity="jaipur";
+console.log(accountId);
+accountCity="bengalore";
+let accountState;
+console.table([accountId,accountName,accountCity,accountState]);
